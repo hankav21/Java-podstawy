d71cd362601969b7396755a121f2e143f80c2f05
hankav21 <hankav21@o2.pl>
2023-10-22 16:52:27 +0200

    strukturalnie

--- 015 Enkapsulacja i hermetyzacja/ZnizkiApp.java
+++ 015 Enkapsulacja i hermetyzacja/ZnizkiApp.java
@@ -1,5 +1,20 @@
 public class ZnizkiApp {
     public static void main(String[] args) {
-        
+        String imie, nazwisko;
+        boolean klientPremium = true;
+        double cena = 1100;
+        double cenaObnizona = cena;
+        if (klientPremium && cena > 1000) {
+            //cenaObnizona = cenaObnizona -0.15 * cenaObnizona;
+            cenaObnizona *= 0.85;
+        } else if (cena > 1000) {
+            cenaObnizona *= 0.9;
+        } else if (klientPremium) {
+            cenaObnizona *= 0.95;
+        }
+
+        System.out.println("dzieki rabatowi zaoszczedzone zostalo: " + (cena - cenaObnizona) +  " zł");
+
+
     }
 }
