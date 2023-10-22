9ee2955097d8b8493e76d2654266daf2e2451958
hankav21 <hankav21@o2.pl>
2023-10-22 17:13:14 +0200

    dodanie znizkowania

--- 015 Enkapsulacja i hermetyzacja/ZnizkiService.java
+++ 015 Enkapsulacja i hermetyzacja/ZnizkiService.java
@@ -8,14 +8,17 @@ public class ZnizkiService {
 
     private double obliczStandardowaZnizke (double cena){
 
-        if (cena > 1000) return cena * 0.9;
+        if (cena > 1000) return przyznajZnizke(cena,0.1);
         return cena;
     }
 
     private double obliczZnizkePremium(double cena){
 
-        if (cena > 1000) return cena * 0.85;
+        if (cena > 1000) return przyznajZnizke(cena, 0.15);
         return  cena * 0.9;
+    }
 
+    private double przyznajZnizke(double cena, double znizka){
+        return  cena * (1 - znizka);
     }
 }
