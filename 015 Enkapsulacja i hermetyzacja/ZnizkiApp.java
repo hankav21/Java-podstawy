49919756d24c6e3b76ec0c85e954096147d4381f
hankav21 <hankav21@o2.pl>
2023-10-23 20:32:31 +0200

    odczytywanie ze strumienia

--- 015 Enkapsulacja i hermetyzacja/ZnizkiApp.java
+++ 015 Enkapsulacja i hermetyzacja/ZnizkiApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ZnizkiApp {
     public static void main(String[] args) {
 
@@ -9,6 +11,26 @@ public class ZnizkiApp {
         cenaObnizona = serwisZnizkowy.obliczCenePoObnizce(klient1,cena);
 
         System.out.println("dzieki rabatowi zaoszczedzone zostalo: " + (cena - cenaObnizona) +  " zł");
+
+        Klient klienci[] = new Klient[3];
+        Scanner input = new Scanner(System.in);
+//        System.out.println(klienci[0]); = null
+        for (int i = 0; i < klienci.length; i++) {
+
+            String nazwisko;
+            //alt + enter - dodaje do klasy wsystkie brakujace importy
+            System.out.println("Podaje imie klienta premium: ");
+            String imie = input.nextLine();
+            System.out.println("Podaje nazwisko klienta premium: ");
+            nazwisko = input.nextLine();
+
+            klienci[i] = new Klient(imie, nazwisko, true);
+        }
+
+        input.close();
+        //zamykamy skaner. Jesli mamy kilka ktore korzystaja ze strumienai system.in,
+        // to robiac close zamykamy sobie mozliwość odczytu z konsoli => wtedy przy zamknieciu aki to robic
+        System.out.println(klienci[0].getImie());
 
 
     }
